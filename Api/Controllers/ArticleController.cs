c55938b8a4a71177964768dc35e655072c8a8465
Kevin Jobs <me@kevinjobs.com>
2023-03-15 21:38:27 +0800

    Update ArticleController.cs

--- Api/Controllers/ArticleController.cs
+++ Api/Controllers/ArticleController.cs
@@ -26,5 +26,43 @@ namespace Api.Controllers
 
             return article;
         }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+            ArticleService.Add(article);
+            return CreatedAtAction(nameof(Get), new { id = article.Id }, article);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Article article)
+        {
+            if (id != article.Id)
+            {
+                return BadRequest();
+            }
+
+            var existed = ArticleService.Get(id);
+            if (existed is null)
+            {
+                return NotFound();
+            }
+
+            ArticleService.Update(article);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var article = ArticleService.Get(id);
+            if (article is null)
+                return NotFound();
+
+            ArticleService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
