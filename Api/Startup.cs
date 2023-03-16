08ef6ef273836f35704e76a5b21c29451b25fe73
Kevin Jobs <me@kevinjobs.com>
2023-03-16 19:31:53 +0800

    refactor: add startup.cs

---
+++ Api/Startup.cs
@@ -0,0 +1,40 @@
+using Api.Data;
+using Api.Services;
+namespace Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<ArticleService>();
+        services.AddSqlite<ArticleContext>("Data Source=Api.db");
+        
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
